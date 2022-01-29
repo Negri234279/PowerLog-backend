50227bb86b56fee2dde3893c94c9c415aa77d676
Negri234279 <rafalejo@live.com>
2022-01-30 00:43:30 +0100

    reafactor index

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,13 @@ const { appPort } = require("./config")
 
 const app = express()
 
+//Middleware
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
-app.use(indexRoutes)
-app.use((err, req, res, next) => res.json({ message: err.message }))
 
+//Routes
+app.use(indexRoutes)
+
+//app.use((err, req, res, next) => res.json({ message: err.message }))
 app.listen(appPort, () => { console.log(`Server running in: localhost:${appPort}`) })
