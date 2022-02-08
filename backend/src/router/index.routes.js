df8af1522e67d1cc578c6d9659319905a2fab93c
Negri234279 <rafalejo@live.com>
2022-02-08 17:29:57 +0100

    add authorize

--- backend/src/router/index.routes.js
+++ backend/src/router/index.routes.js
@@ -1,20 +1,26 @@
 const { Router } = require('express')
-const { registerUser } = require('../controllers/auth.controller')
+const { registerUser, loginUser, verify } = require('../controllers/auth.controller')
+const { profileUser } = require('../controllers/user.controller')
 const { getAllWorkouts, createWorkout, updateWorkout, getWorkoutsByUserID, getWorkoutsById, delWorkoutById } = require('../controllers/workout.controller')
+const auhorize = require('../middleware/auhorize')
 const validInfo = require('../middleware/validInfo')
 
 
 const router = Router()
 
 //Workout
-router.get('/api/workout', getAllWorkouts)
-router.get('/api/workout/:idUser', getWorkoutsByUserID)
-router.get('/api/workout/update/:idWorkout', getWorkoutsById)
-router.post('/api/workout', createWorkout)
-router.delete('/api/workout/:idWorkout', delWorkoutById)
-router.put('/api/workout/:idWorkout', updateWorkout)
+router.get('/api/workout', auhorize, getWorkoutsByUserID)
+router.get('/api/workout/update/:idWorkout', auhorize, getWorkoutsById)
+router.post('/api/workout', auhorize, createWorkout)
+router.delete('/api/workout/:idWorkout', auhorize, delWorkoutById)
+router.put('/api/workout/:idWorkout', auhorize, updateWorkout)
 
 //Auth
 router.post('/api/auth/register', validInfo, registerUser)
+router.post('/api/auth/login', validInfo, loginUser)
+router.post('/api/auth/verify', auhorize, verify)
+
+//User
+router.get('/api/user/profile', auhorize, profileUser)
 
 module.exports = router
