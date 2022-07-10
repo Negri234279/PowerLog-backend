11d4be348b2b14e749b4d11acecafa1859622b78
negri234279 <rafalejo@live.com>
2022-07-10 19:03:32 +0200

    👷 Update cypress config

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -7,5 +7,19 @@ export default defineConfig({
     },
     projectId: "bwaiwx",
     baseUrl: "http://127.0.0.1:3100/api/",
+    video: false,
+    screenshotOnRunFailure: false,
+    browsers: [
+      {
+        name: 'chrome',
+        family: 'chromium',
+        channel: 'stable',
+        displayName: 'Chrome',
+        version: '103.0.5060.114',
+        path: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+        minSupportedVersion: 64,
+        majorVersion: 103,
+      }
+    ],
   },
 })
