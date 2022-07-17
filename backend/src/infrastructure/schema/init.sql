81a1992a00503600c8b4ed414a9f6800aaba77e7
Negri234279 <rafalejo@live.com>
2022-07-17 16:52:16 +0200

    🌱  Update to insert workout data

--- backend/src/infrastructure/schema/init.sql
+++ backend/src/infrastructure/schema/init.sql
@@ -11,12 +11,18 @@ VALUES ('0f476be6-b0cf-4984-90e7-ad2d7041cf0e', 'test', 'test@test.com', '$2b$10
 
 
 CREATE TABLE workouts (
-    id_workouts SERIAL PRIMARY KEY,
+    id_workouts VARCHAR(255) PRIMARY KEY,
     workout VARCHAR(255) NOT NULL,
     w_weight INT NOT NULL,
     w_reps INT NOT NULL,
     w_sets INT NOT NULL,
-    w_date VARCHAR(255) NOT NULL,
+    w_date DATE NOT NULL,
     id_user VARCHAR(255) NOT NULL,
     FOREIGN KEY (id_user) REFERENCES users(id_user)
 );
+
+INSERT INTO workouts (id_workouts, workout, w_weight, w_reps, w_sets, w_date, id_user)
+VALUES ('0f476be6-b0cf-4984-90e7-ad2d7041cf0a', 'SQHG', 110, 4, 4, '01/01/2022', '0f476be6-b0cf-4984-90e7-ad2d7041cf0e');
+
+INSERT INTO workouts (id_workouts, workout, w_weight, w_reps, w_sets, w_date, id_user)
+VALUES ('0f476be6-b0cf-4984-90e7-ad2d7041cf0b', 'BP', 50, 6, 4, '01/01/2022', '0f476be6-b0cf-4984-90e7-ad2d7041cf0e');
