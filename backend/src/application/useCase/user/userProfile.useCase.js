80b886eeb513a65a5c7b088a2eebb9fdfd006af2
Negri234279 <rafalejo@live.com>
2022-07-17 16:49:38 +0200

    ⚰️ Remove import not uses

--- backend/src/application/useCase/user/userProfile.useCase.js
+++ backend/src/application/useCase/user/userProfile.useCase.js
@@ -1,7 +1,4 @@
-import { VOFormatException } from '../../../domain/errors/voFormat.exeption.js'
 import { VOUuid } from '../../../domain/valueObject/shared/uuid.vo.js'
-import { VOPlainPassword } from '../../../domain/valueObject/user/plainPassword.vo.js'
-import { IdAlreadyInUseException } from '../../errors/shared/idAlredyInUse.exeption.js'
 import { UserMap } from '../../mappers/user.map.js'
 
 export class userProfileUseCase {
