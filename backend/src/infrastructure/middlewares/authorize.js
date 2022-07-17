cc56da4f444647cac895bca86e91b7fa01e03320
Negri234279 <rafalejo@live.com>
2022-07-17 16:50:03 +0200

    🔒️ Add control to bearer string

--- backend/src/infrastructure/middlewares/authorize.js
+++ backend/src/infrastructure/middlewares/authorize.js
@@ -4,10 +4,11 @@ import { InvalidTokenException } from '../errors/invalidToken.exeption.js'
 import { MissingTokenException } from '../errors/missingToken.exception.js'
 
 export default function (req, res, next) {
-    const token = req.header(JWT_NAME)
+    const token = req.header('authorization')
 
     try {
         if (!token) throw new MissingTokenException()
+        if (token.split(' ')[0] !== 'Bearer') throw new InvalidTokenException()
 
         const bearer = token.split(' ')[1]
 
