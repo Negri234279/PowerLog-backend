52fb38e5a208b770bdca98fa9e283158d925fa71
negri234279 <rafalejo@live.com>
2022-07-19 23:56:10 +0200

    👔 Add check if exist user id

--- backend/src/application/useCase/workout/workoutCreate.useCase.js
+++ backend/src/application/useCase/workout/workoutCreate.useCase.js
@@ -6,15 +6,20 @@ import { VOReps } from '../../../domain/valueObject/workout/reps.vo.js'
 import { VOSets } from '../../../domain/valueObject/workout/sets.vo.js'
 import { VOWeight } from '../../../domain/valueObject/workout/weight.vo.js'
 import { IdAlreadyInUseException } from '../../errors/shared/idAlredyInUse.exeption.js'
+import { InvalidLoginException } from '../../errors/user/invalidLogin.exeption.js'
 
 export class workoutCreateUseCase {
-    constructor({ workoutRepository }) {
+    constructor({ workoutRepository, userRepository }) {
         this.workoutRepository = workoutRepository
+        this.userRepository = userRepository
     }
 
     async execute(id, name, sets, reps, weight, date, idUser) {
         const workoutId = new VOUuid(id)
         const userId = new VOUuid(idUser)
+
+        const user = await this.userRepository.findById(userId)
+        if (!user) throw new InvalidLoginException()
 
         const existWorkoutById = await this.workoutRepository.findById(workoutId, userId)
         if (existWorkoutById) throw new IdAlreadyInUseException()
--- backend/src/application/useCase/workout/workoutUpdate.useCase.js
+++ backend/src/application/useCase/workout/workoutUpdate.useCase.js
@@ -6,15 +6,20 @@ import { VOReps } from '../../../domain/valueObject/workout/reps.vo.js'
 import { VOSets } from '../../../domain/valueObject/workout/sets.vo.js'
 import { VOWeight } from '../../../domain/valueObject/workout/weight.vo.js'
 import { IdIsNotFoundException } from '../../errors/shared/IdIsNotFound.exeption.js'
+import { InvalidLoginException } from '../../errors/user/invalidLogin.exeption.js'
 
 export class workoutUpdateUseCase {
-    constructor({ workoutRepository }) {
+    constructor({ workoutRepository, userRepository }) {
         this.workoutRepository = workoutRepository
+        this.userRepository = userRepository
     }
 
     async execute(id, name, sets, reps, weight, date, idUser) {
         const workoutId = new VOUuid(id)
         const userId = new VOUuid(idUser)
+
+        const user = await this.userRepository.findById(userId)
+        if (!user) throw new InvalidLoginException()
 
         const workout = await this.workoutRepository.findById(workoutId, userId)
         if (!workout) throw new IdIsNotFoundException()
