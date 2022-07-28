b6f63846272f929faa85ba0c758669962117fc4b
negri234279 <rafalejo@live.com>
2022-07-28 20:24:36 +0200

    👔 Add workout examples

--- src/infrastructure/schema/init.sql
+++ src/infrastructure/schema/init.sql
@@ -1,3 +1,5 @@
+set datestyle to 'ISO, DMY';
+
 CREATE TABLE users (
     id_user VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
@@ -29,3 +31,20 @@ VALUES ('0f476be6-b0cf-4984-90e7-ad2d7041cf0b', 'BP', 50, 6, 4, '01/01/2022', '0
 
 INSERT INTO workouts (id_workouts, workout, w_weight, w_reps, w_sets, w_date, id_user)
 VALUES ('45e16863-0ec8-41ba-9118-f987ebb0c9cb', 'DL', 110, 5, 5, '01/01/2022', '0f476be6-b0cf-4984-90e7-ad2d7041cf0e');
+
+
+
+INSERT INTO workouts (id_workouts, workout, w_weight, w_sets, w_reps, w_date, id_user)
+VALUES ('5ceafaed-4a22-495d-9981-1b588effcc0f', 'BP Pines', 45, 3, 8, '28/07/2022', '0f476be6-b0cf-4984-90e7-ad2d7041cf0e');
+
+INSERT INTO workouts (id_workouts, workout, w_weight, w_sets, w_reps, w_date, id_user)
+VALUES ('874b5e20-1728-4cfa-84d0-00423c5568ca', 'DL BLoques', 100, 4, 6, '30/07/2022', '0f476be6-b0cf-4984-90e7-ad2d7041cf0e');
+
+INSERT INTO workouts (id_workouts, workout, w_weight, w_sets, w_reps, w_date, id_user)
+VALUES ('8a17d214-b7ac-45c0-be95-cf28a25fa1db', 'DL', 90, 4, 8, '31/07/2022', '0f476be6-b0cf-4984-90e7-ad2d7041cf0e');
+
+INSERT INTO workouts (id_workouts, workout, w_weight, w_sets, w_reps, w_date, id_user)
+VALUES ('0591bb86-30ad-4642-9f8c-a2f9b6f00c7e', 'SQHB', 90, 4, 8, '31/07/2022', '0f476be6-b0cf-4984-90e7-ad2d7041cf0e');
+
+INSERT INTO workouts (id_workouts, workout, w_weight, w_sets, w_reps, w_date, id_user)
+VALUES ('7099ef21-1db5-4e77-aff9-09f72ab6bb89', 'SQHB Pines', 75, 3, 6, '27/07/2022', '0f476be6-b0cf-4984-90e7-ad2d7041cf0e');
